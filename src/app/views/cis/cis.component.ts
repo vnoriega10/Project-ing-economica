a375b2c89d841bdded7f771b4333603631acaf1c
Victor Noriega <79641120+vnoriega10@users.noreply.github.com>
2023-09-08 11:50:15 -0500

    Actualizado 08/09/2023

--- src/app/views/cis/cis.component.ts
+++ src/app/views/cis/cis.component.ts
@@ -6,13 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./cis.component.css']
 })
 export class CisComponent {
-
-  capitalSim: string = '';
-  interestSim: string = '';
-  yearsSim: string = '';
-  monthsSim: string  = '';
-  daysSim: string = '';
-  totalInterestSim: number= 0;
+  interesesSim :number = 0;
+  capitalSim: number = 0;
+  interestSim: number = 0;
+  yearsSim: number = 0;
+  monthsSim: number = 0;
+  daysSim:number = 0;
+  totalInterestSim: number = 0;
   totalVSim: number = 0;
   mostrarInputsDate: boolean = false;
 
@@ -21,16 +21,24 @@ export class CisComponent {
   }
 
   calculateInterestSim(): void {
-     // Approximate days to months
-    const totaldiassim = (parseInt(this.yearsSim) * 365) + (parseInt(this.monthsSim) * 30) + this.daysSim;
-    const interestsim = ((parseInt(this.capitalSim)) * (1 + ((parseInt(this.interestSim)/100) * (parseInt(totaldiassim)/365))));
-    this.totalInterestSim = interestsim - parseInt(this.capitalSim);
-    this.totalVSim = this.totalInterestSim + parseInt(this.capitalSim);
+    // Verificar si los campos necesarios para el cálculo están llenos
+    if (this.interesesSim && this.interestSim && this.monthsSim && this.daysSim && this.yearsSim){
+      const totaldias = (this.yearsSim + (this.monthsSim/12) + (this.daysSim/360));
+      this.capitalSim =this.interesesSim / ((this.interestSim/100) * totaldias); //i/it
+      this.totalVSim = this.capitalSim + this.interesesSim;
+
+    } else {
+        // Realiza el cálculo del interés y el capital como lo hacías antes
+        const totaldiassim = (this.yearsSim * 365) + (this.monthsSim * 30) + this.daysSim;
+        const interestsim = ((this.capitalSim) * (1 + ((this.interestSim / 100) * (totaldiassim / 365))));
+        this.totalInterestSim = interestsim - this.capitalSim;
+        this.totalVSim = this.totalInterestSim + this.capitalSim;
+    }
   }
 
-  get formattedTotalInterestSim(): string {
-    const formattedValue = this.totalInterestSim.toFixed(2); // Formato con dos decimales
-    return `$${formattedValue}`; // Concatenar símbolo "$" con el valor
+  get formattedTotalInterestSim(): number {
+    const formattedValue = this.totalInterestSim; // Formato con dos decimales // Concatenar símbolo "$" con el valor
+    return formattedValue; // Concatenar símbolo "$" con el valor
   }
 
   get formattedTotalSim(): string {
--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent {
+
+  intereses : number = 0;
   capital: number = 0;
   interestRate: number = 0;
   years: number = 0;
@@ -15,16 +17,25 @@ export class MainComponent {
   totalV: number = 0;
 
   calculateInterest(): void {
+
+    if(this.intereses && this.interestRate && this.months && this.days && this.years){
+      const totaldias = (this.years + (this.months/12) + (this.days/360));
+      this.capital =this.intereses / ((this.interestRate/100) * totaldias); //i/it
+      this.totalV = this.capital + this.intereses;
+    }else{
+      const totaldias = (this.years * 365) + (this.months * 30) + this.days;
+      const interest = ((this.capital) * (1 + ((this.interestRate/100) * (totaldias/365))));
+      this.totalInterest = interest - this.capital;
+      this.totalV = this.totalInterest + this.capital;
+    }   
      // Approximate days to months
-    const totaldias = (this.years * 365) + (this.months * 30) + this.days;
-    const interest = ((this.capital) * (1 + ((this.interestRate/100) * (totaldias/365))));
-    this.totalInterest = interest - this.capital;
-    this.totalV = this.totalInterest + this.capital;
+    
   }
 
-  get formattedTotalInterest(): string {
-    const formattedValue = this.totalInterest.toFixed(1); // Formato con dos decimales
-    return `$${formattedValue}`; // Concatenar símbolo "$" con el valor
+  get formattedTotalInterest(): number {
+
+    const formattedValue = this.totalInterest; // Formato con dos decimales // Concatenar símbolo "$" con el valor
+    return formattedValue;
   }
 
   get formattedTotal(): string {

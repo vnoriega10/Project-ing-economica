318947b1ba80d8352d6a6a8bfcf22eae66f5aef7
Victor Noriega <79641120+vnoriega10@users.noreply.github.com>
2023-09-18 00:23:21 -0500

    actualizado

--- src/app/views/cic/cic.component.ts
+++ src/app/views/cic/cic.component.ts
@@ -6,5 +6,93 @@ import { Component } from '@angular/core';
   styleUrls: ['./cic.component.css']
 })
 export class CicComponent {
+  capitalInicial: number = 0;
+  tasaInteres: number = 0;
+  montoFinal: number = 0;
+  periodo: string = 'diario'; // Valor por defecto
+  tiempo: number = 1; // Valor por defecto
+  interesesCom :number = 0;
+
+  interesCompuestoCalculado: number | undefined;
+
+  calcularInteresCompuesto(){
+    //calcular montofinal
+    const tasaDecimal = this.tasaInteres/100;
+    if(this.capitalInicial && this.tiempo && this.tasaInteres && this.periodo ){
+      if(this.periodo == "diario"){
+        this.montoFinal = this.capitalInicial * Math.pow(1 + tasaDecimal, (this.tiempo/30));
+        this.interesesCom = this.montoFinal - this.capitalInicial;
+      }else if(this.periodo =="meses"){
+        this.montoFinal = this.capitalInicial * Math.pow(1 + tasaDecimal, (this.tiempo));
+        this.interesesCom = this.montoFinal - this.capitalInicial;
+      }else if(this.periodo =="trimestral"){
+        this.montoFinal = this.capitalInicial * Math.pow(1 + tasaDecimal, (this.tiempo/3));
+        this.interesesCom = this.montoFinal - this.capitalInicial;
+      }else if(this.periodo =="cuatrimestral"){
+        this.montoFinal = this.capitalInicial * Math.pow(1 + tasaDecimal, (this.tiempo/4));
+        this.interesesCom = this.montoFinal - this.capitalInicial;
+      }else if(this.periodo =="semestral"){
+        this.montoFinal = this.capitalInicial * Math.pow(1 + tasaDecimal, (this.tiempo/6));
+        this.interesesCom = this.montoFinal - this.capitalInicial;
+      }else if(this.periodo =="anual"){
+        this.montoFinal = this.capitalInicial * Math.pow(1 + tasaDecimal, (this.tiempo));
+        this.interesesCom = this.montoFinal - this.capitalInicial;
+      }
+    }else if(this.tasaInteres && this.tiempo && this.montoFinal && this.periodo){
+      //calcular el capital inicial
+      if(this.periodo == "Diario"){
+        this.capitalInicial = this.montoFinal / Math.pow(1 + tasaDecimal, (this.tiempo/365));
+        this.interesesCom = this.montoFinal - this.capitalInicial;
+      }else if(this.periodo =="meses"){
+        this.capitalInicial = this.montoFinal / Math.pow(1 + tasaDecimal, this.tiempo);
+        this.interesesCom = this.montoFinal - this.capitalInicial;
+      }else if(this.periodo =="trimestral"){
+        this.capitalInicial = this.montoFinal / Math.pow(1 + tasaDecimal, (this.tiempo/3));
+        this.interesesCom = this.montoFinal - this.capitalInicial;
+      }else if(this.periodo =="cuatrimestral"){
+        this.capitalInicial = this.montoFinal / Math.pow(1 + tasaDecimal, (this.tiempo/4));
+        this.interesesCom = this.montoFinal - this.capitalInicial;
+      }else if(this.periodo =="semestral"){
+        this.capitalInicial = this.montoFinal / Math.pow(1 + tasaDecimal, (this.tiempo/6));
+        this.interesesCom = this.montoFinal - this.capitalInicial;
+      }else if(this.periodo =="anual"){
+        this.capitalInicial = this.montoFinal / Math.pow(1 + tasaDecimal, (this.tiempo/1));
+        this.interesesCom = this.montoFinal - this.capitalInicial;
+      }
+    }else if(this.tasaInteres && this.capitalInicial && this.montoFinal){
+      //calcular el tiempo
+      this.tiempo = (Math.log(this.montoFinal) - Math.log(this.capitalInicial)) / Math.log(1 + tasaDecimal);
+      this.periodo = "meses";
+      this.interesesCom = this.montoFinal - this.capitalInicial;
+    }else if (this.capitalInicial && this.montoFinal && this.periodo) {
+      //Calcular la tasaInteres
+      if(this.periodo == "diario"){
+        this.tasaInteres = ((this.montoFinal / this.capitalInicial) ** (1 / (this.tiempo/365)) - 1) * 100;
+        this.interesesCom = this.montoFinal - this.capitalInicial;
+      }else if(this.periodo =="meses"){
+        this.tasaInteres = ((this.montoFinal / this.capitalInicial) ** (1 / this.tiempo) - 1) * 100;
+        this.interesesCom = this.montoFinal - this.capitalInicial;
+      }else if(this.periodo =="trimestral"){
+        this.tasaInteres = ((this.montoFinal / this.capitalInicial) ** (1 / (this.tiempo/3)) - 1) * 100;
+        this.interesesCom = this.montoFinal - this.capitalInicial;
+      }else if(this.periodo =="cuatrimestral"){
+        this.tasaInteres = ((this.montoFinal / this.capitalInicial) ** (1 / (this.tiempo/4)) - 1) * 100;
+        this.interesesCom = this.montoFinal - this.capitalInicial;
+      }else if(this.periodo =="semestral"){
+        this.tasaInteres = ((this.montoFinal / this.capitalInicial) ** (1 / (this.tiempo/6)) - 1) * 100;
+        this.interesesCom = this.montoFinal - this.capitalInicial;
+      }else if(this.periodo =="anual"){
+        this.tasaInteres = ((this.montoFinal / this.capitalInicial) ** (1 / this.tiempo) - 1) * 100;
+        this.interesesCom = this.montoFinal - this.capitalInicial;
+      }
+    }
+  }
+
+
+
+  get formattedTotalCom(): string {
+    const formattedValue = this.interesesCom.toFixed(3); // Formato con dos decimales
+    return `$${formattedValue}`; // Concatenar símbolo "$" con el valor
+  }
 
 }
--- src/app/views/cis/cis.component.ts
+++ src/app/views/cis/cis.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./cis.component.css']
 })
 export class CisComponent {
+  inicio : string = '';
+  final: string = '';
   interesesSim :number = 0;
   capitalSim: number = 0;
   interestSim: number = 0;
@@ -22,27 +24,71 @@ export class CisComponent {
 
   calculateInterestSim(): void {
     // Verificar si los campos necesarios para el cálculo están llenos
-    if (this.interesesSim && this.interestSim && this.monthsSim && this.daysSim && this.yearsSim){
-      const totaldias = (this.yearsSim + (this.monthsSim/12) + (this.daysSim/360));
-      this.capitalSim =this.interesesSim / ((this.interestSim/100) * totaldias); //i/it
-      this.totalVSim = this.capitalSim + this.interesesSim;
+    if (this.interesesSim !==0 && this.interestSim !==0){
+      if(this.capitalSim===0 && this.yearsSim !==0 || this.monthsSim !==0 || this.daysSim !==0){
+        const totaldias = (this.yearsSim + (this.monthsSim/12) + (this.daysSim/360));
+        this.capitalSim =parseFloat((this.interesesSim / ((this.interestSim / 100) * totaldias)).toFixed(1)); //i/it
+        this.totalVSim = this.capitalSim + this.interesesSim;
+        console.log("calculo capital");
+      }
 
-    } else {
-        // Realiza el cálculo del interés y el capital como lo hacías antes
+    } else if(this.capitalSim !==0 && this.interesesSim !==0)  {
+      if(this.interestSim===0 && this.yearsSim !==0 || this.monthsSim !==0 || this.daysSim !==0 ){
+        const totaldias = (this.yearsSim + (this.monthsSim/12) + (this.daysSim/360));
+        this.interestSim = parseFloat(((this.interesesSim / (this.capitalSim* totaldias))*100).toFixed(1));
+        this.totalVSim = this.capitalSim + this.interesesSim;
+        console.log("calculo tasa interés");
+      }
+
+    } else if(this.capitalSim!==0 && this.interestSim!==0) {
+      if(this.interesesSim===0 && this.yearsSim !==0 || this.monthsSim !==0 || this.daysSim !==0){
         const totaldiassim = (this.yearsSim * 365) + (this.monthsSim * 30) + this.daysSim;
-        const interestsim = ((this.capitalSim) * (1 + ((this.interestSim / 100) * (totaldiassim / 365))));
+        const interestsim = ((this.capitalSim) * (1 + ((this.interestSim / 100) * (totaldiassim / 360))));
         this.totalInterestSim = interestsim - this.capitalSim;
         this.totalVSim = this.totalInterestSim + this.capitalSim;
+        console.log("calculo intereses")
+      }
+        // Realiza el cálculo del interés y el capital como lo hacías antes
+        
+    } 
+    
+    if(this.capitalSim && this.interesesSim && this.interestSim){
+      if(this.daysSim===0 && this.yearsSim ===0 && this.monthsSim===0){
+        this.yearsSim = Math.floor(parseFloat((this.interesesSim/(this.capitalSim * (this.interestSim/100))).toFixed(1)));
+        const entero = this.yearsSim;
+        const decimales = parseFloat((this.interesesSim/(this.capitalSim * (this.interestSim/100))).toFixed(5)) - entero ;
+        const decimalesmes = Math.round(decimales * 360);
+        this.monthsSim = Math.floor(decimalesmes/30);
+        const valordias =  this.monthsSim * 30
+        this.daysSim = decimalesmes - valordias;
+        this.totalVSim = this.interesesSim + this.capitalSim;
+      }
     }
+
+
+    if(this.inicio && this.final){
+      const fechaInicio = new Date(this.inicio);
+      const fechaFinal = new Date(this.final);
+
+      const milisegundos = 24*60*60*1000;
+      const diferenciasDias = Math.round((fechaFinal.getTime() - fechaInicio.getTime())/milisegundos)
+
+      const interestsim = ((this.capitalSim) * (1 + ((this.interestSim / 100) * (diferenciasDias/ 360))));
+      this.totalInterestSim = interestsim - this.capitalSim;
+      this.totalVSim = this.capitalSim + this.totalInterestSim;
+      
+    }
+
+    
   }
 
   get formattedTotalInterestSim(): number {
-    const formattedValue = this.totalInterestSim; // Formato con dos decimales // Concatenar símbolo "$" con el valor
-    return formattedValue; // Concatenar símbolo "$" con el valor
+    const formattedValue = parseFloat(this.totalInterestSim.toFixed(1));
+    return formattedValue;
   }
 
   get formattedTotalSim(): string {
-    const formattedValue = this.totalVSim.toFixed(3); // Formato con dos decimales
+    const formattedValue = this.totalVSim.toFixed(1); // Formato con dos decimales
     return `$${formattedValue}`; // Concatenar símbolo "$" con el valor
   }
 
